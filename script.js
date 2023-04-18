c73a1f30272ade8f8c87d943032b4af9c5cde5fd
KKaged <dhigaredaleon707@gmail.com>
2023-04-18 18:39:28 +0900

    added hover effect. not done

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-let input = document.getElementById(`quantity`);
-function submit() {
-  console.log("test");
+const container = document.querySelector(".grid-container");
+const gridCell = document.createElement("div");
+
+function gridCells() {
+  for (let i = 0; i < 16; i++) {
+    const gridCell = document.createElement("div");
+    gridCell.classList.add("grid-cell");
+    container.appendChild(gridCell);
+    gridCell.style.display = "flex";
+    gridCell.style.flexDirection = "column";
+  }
 }
+
+gridCells();
+gridCells2();
