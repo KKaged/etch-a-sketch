d3ad379175864826a12c2f24019554d7f32a375c
KKaged <dhigaredaleon707@gmail.com>
2023-04-19 17:19:26 +0900

    fixed grid where it updates with input

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const container = document.querySelector(".grid-container");
 const gridCell = document.createElement("div");
 const gridSize = document.getElementById("quantity");
 
+gridSize.addEventListener("input", function () {
+  console.log(gridSize.value);
+  updateGrid();
+});
+
 function gridCells() {
   for (let i = 0; i < 256; i++) {
     const gridCell = document.createElement("div");
@@ -9,5 +14,23 @@ function gridCells() {
     container.appendChild(gridCell);
   }
 }
-gridCells();
-console.log(gridSize.value);
+
+updateGrid = () => {
+  container.innerHTML = "";
+  container.style.setProperty(
+    "grid-template-columns",
+    `repeat(${gridSize.value},2fr)`
+  );
+
+  container.style.setProperty(
+    "grid-template-rows",
+    `repeat(${gridSize.value},2fr)`
+  );
+
+  for (let i = 0; i < gridSize.value * gridSize.value; i++) {
+    const gridCell = document.createElement("div");
+    gridCell.classList.add("grid-cell");
+    container.appendChild(gridCell);
+  }
+};
+gridCell();
