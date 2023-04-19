1f6265a64b0923b514d0b5817d8e92762937f757
KKaged <dhigaredaleon707@gmail.com>
2023-04-19 17:02:34 +0900

    fixed Grid.

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 const container = document.querySelector(".grid-container");
 const gridCell = document.createElement("div");
+const gridSize = document.getElementById("quantity");
 
 function gridCells() {
-  for (let i = 0; i < 16; i++) {
+  for (let i = 0; i < 256; i++) {
     const gridCell = document.createElement("div");
     gridCell.classList.add("grid-cell");
     container.appendChild(gridCell);
-    gridCell.style.display = "flex";
-    gridCell.style.flexDirection = "column";
   }
 }
-
 gridCells();
-gridCells2();
+console.log(gridSize.value);
